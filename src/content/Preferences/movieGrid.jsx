37f043574e15213bea78375e0cf0a3993a64c278
Shahan M <mehtabi@clemson.edu>
2021-03-01 12:30:45 -0500

    Add Carousel to the MovieGrid page

--- src/content/Preferences/movieGrid.jsx
+++ src/content/Preferences/movieGrid.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import StarRatings from 'react-star-ratings';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import axios from "axios";
 
 const Movie = props => (
@@ -14,7 +16,7 @@ const Movie = props => (
 	  <td>{props.movie.aveRating}</td>
 	  <td>{props.movie.director}</td>
 	  <td>{props.movie.writer}</td>
-	  {/* <td>{props.movie.description}</td> */}
+	  <td>{props.movie.description}</td>
 	  <td>{props.movie.cast}</td>
 	  <td>
 		<img src={props.movie.poster} alt={props.movie.title} width="100" />
@@ -22,19 +24,43 @@ const Movie = props => (
 	</tr>
   );
 
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3 // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    }
+  };
+
 class MovieGrid extends Component {
 	
 	constructor(props) {
 		super(props)
 		this.state = {
-		  rating: 0,
-		  movies: [],
-		  mId: ""
+			movies: [],
+			movies_: [],
+			visited: []
 		}
-	  }
+	}
 
-	  componentDidMount() {
+	componentDidMount() {
 		var API = "";
+		var movie_map = [];
 		if (process.env.NODE_ENV === "production") {
 		  API = "https://movie-mern.herokuapp.com/api/movies/";
 		} else {
@@ -43,17 +69,22 @@ class MovieGrid extends Component {
 		axios
 		  .get(API)
 		  .then(response => {
-			this.setState({ movies: response.data });
+			  response.data.map(movie => {
+				  movie_map.push({
+					  "movie": movie,
+					  "rating": 0
+				  });
+			  },
+			this.setState({movies: response.data,
+							movies_: movie_map})
+			  );
 		  })
 		  .catch(error => {
 			console.log(error);
 		  });
-	  }
+	}
   
-  
-	  
-  
-	  movieList() {
+	movieList() {
 		return this.state.movies.map(currentmovie => {
 		  return (
 			<Movie
@@ -63,15 +94,17 @@ class MovieGrid extends Component {
 			/>
 		  );
 		});
-	  }
+	}
 	
-	  onChangeMovieId(e) {
+	onChangeMovieId(e) {
+		  console.log("onChangeMovieId");
+		  console.log(e);
 		this.setState({
 		  mId: e.target.value
 		});
-	  }
+	}
 	
-	  onSubmit(e) {
+	onSubmit(e) {
 		e.preventDefault();
 	
 		// const exercise = {
@@ -91,61 +124,128 @@ class MovieGrid extends Component {
 		axios
 		  .get(API + this.state.mId)
 		  .then(response => {
-			this.setState({ movies: response.data });
+			this.setState({ movies: response.data});
 		  })
 		  .catch(error => {
 			console.log(error);
 		  });
-	  }
-
-	  
+	} 
 	
-	changeRating( newRating ) {
+	// changeRating( newRating ) {
+	// 	console.log(newRating);
+	// 	// console.log(currentmovie);
+	// 	this.setState({
+	// 	  rating: newRating
+	// 	});
+	// }
+	
+	changeRating = (newRating, movieid) =>{
+		console.log(movieid);
+		console.log(this.state.movies_[movieid]);
+		console.log(newRating);
 		this.setState({
 		  rating: newRating
 		});
-	  }
-	
+		this.props.handler();
+	}
 
-    render() { 
-        return ( 
-            <div>
-                <div class="movie-block" id="movie_10">
-					<div class="container">
-						
-						{this.state.movies.slice(50, 55).map(currentmovie => ( 
-						<div class="wrapper-block"  key={currentmovie._id}>
-						
+	getRandomMovies = () => {
+		let allMovies = this.state.movies_;
+		const randomCount = 5;
 
+		let movieMap = {};
+		const randomMovies = [];
+		for (let i = 0; i < randomCount; i++){
+			const randIdx = Math.floor(Math.random() * allMovies.length);
+			const randItm = allMovies.splice(randIdx, 1)[0];
+			// randomMovies[randItm._id] = randItm;
+			randomMovies.push(randItm);
+			// this.setState({
+			// 	visited: randomMovies
+			// });
+			movieMap[randItm.movie._id] = randItm;
+			console.log(randItm);
+		}
+		console.log(movieMap);
+		return randomMovies;
+	}
 
-							<div class="img-block" > 
+	getRandomMovie = () => {
+		let allMovies = this.state.movies_;
+		const randomCount = 5;
+
+		const randIdx = Math.floor(Math.random() * allMovies.length);
+		return allMovies.splice(randIdx, 1)[0];
+	}
+
+	removeItem = (movieID) => {
+		console.log(movieID);
+	}
+
+    render() {
+		if (this.state.movies_.length > 0){
+			let randomMovies = [];
+			if (this.state.visited.length <= 5){
+				randomMovies = this.getRandomMovies();
+
+			console.log(randomMovies);
+			} else {
+				randomMovies = this.state.visited;
+			}
+			console.log(randomMovies);
+			return (
+				<div>
+					<Carousel  
+					additionalTransfrom={0}
+					arrows
+					autoPlaySpeed={3000}
+					centerMode={true}
+					className=""
+					containerClass="container-with-dots"
+					dotListClass=""
+					draggable
+					focusOnSelect={false}
+					infinite
+					keyBoardControl
+					minimumTouchDrag={80}
+					renderButtonGroupOutside={false}
+					renderDotsOutside={false}
+					responsive={responsive}		
+					showDots={false}
+					sliderClass=""
+					slidesToSlide={1}
+					swipeable
+					itemClass="carousel-item-padding-0-px">
+					{randomMovies.map(currentmovie => (
+						<div class="container"  key={currentmovie.movie._id}>
 							
-						
-					   			<img id="TN_1" src={currentmovie.poster} className="imageTrans"/>
-					   
-					   			<div class="middle">
-					   			<StarRatings
-									rating={this.state.rating}
-									starRatedColor="rgb(252,229,65)"
-									starHoverColor="rgb(252,229,65)"
-									starDimension="15px"
-									starSpacing="2px"
-									changeRating={this.changeRating}
-									numberOfStars={5}
-									name='rating'/>
-									<div class="text"> {currentmovie.title}</div>
+							<img id={"TN_"+currentmovie.movie._id} src={currentmovie.movie.poster} className="imageTrans"/>
+								<div class="star-div">
+									<StarRatings
+										rating={currentmovie.rating}
+										starRatedColor="rgb(252,229,65)"
+										starHoverColor="rgb(252,229,65)"
+										starDimension="27px"
+										starSpacing="3px"
+										changeRating={this.changeRating}
+										numberOfStars={5}
+										name={currentmovie.movie._id}/>
 								</div>
-								
-								
-							</div>										
-						</div> ))}
-						 {/*wrapper ending */}
-					</div>
-				
+								<div class="text">
+									{currentmovie.movie.title}
+								</div>									
+						</div>
+						// <div>
+						// <img src={currentmovie.movie.poster} className="imageTrans"/>
+						// </div>
+					))}
+					</Carousel>
 				</div>
-            </div>
-         );
+			);
+		} else {
+			return (<div></div>);
+		}
     }
 }
- 
+
 export default MovieGrid;
--- src/content/carousel.jsx
+++ src/content/carousel.jsx
@@ -1,87 +1,78 @@
 import React, { Component } from 'react';
 import Carousel from "react-multi-carousel";
-import MovieGrid  from "./Preferences/MovieGrid.jsx";
+import MovieGrid  from "./Preferences/movieGrid.jsx";
 import WithStyles from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
-class CarouselClass extends Component {
-   
-
-
-    render() { 
-        const responsive = {
-            desktop: {
-              breakpoint: { max: 3000, min: 1024 },
-              items: 3,
-              slidesToSlide: 3 // optional, default to 1.
-            },
-            tablet: {
-              breakpoint: { max: 1024, min: 464 },
-              items: 2,
-              slidesToSlide: 2 // optional, default to 1.
-            },
-            mobile: {
-              breakpoint: { max: 464, min: 0 },
-              items: 1,
-              slidesToSlide: 1 // optional, default to 1.
-            }
-          };
-
-        return (
-<div>
-<Carousel  additionalTransfrom={0}
-  arrows
-  autoPlaySpeed={3000}
-  centerMode={false}
-  className=""
-  containerClass="container-with-dots"
-  dotListClass=""
-  draggable
-  focusOnSelect={false}
-  infinite
-  itemClass=""
-  keyBoardControl
-  minimumTouchDrag={80}
-  renderButtonGroupOutside={false}
-  renderDotsOutside={false}
-  responsive={{
-    desktop: {
-      breakpoint: {
-        max: 3000,
-        min: 1024
-      },
-      items: 3,
-      partialVisibilityGutter: 40
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
     },
-    mobile: {
-      breakpoint: {
-        max: 464,
-        min: 0
-      },
-      items: 1,
-      partialVisibilityGutter: 30
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3 // optional, default to 1.
     },
     tablet: {
-      breakpoint: {
-        max: 1024,
-        min: 464
-      },
+      breakpoint: { max: 1024, min: 464 },
       items: 2,
-      partialVisibilityGutter: 30
+      slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
     }
-  }}
-  showDots={false}
-  sliderClass=""
-  slidesToSlide={1}
-  swipeable >
-<div><MovieGrid /></div>
-<div>Item 2</div>
-</Carousel>
+  };
 
-</div>
+class CarouselClass extends Component {
 
-          );
-    }
+  render() {
+    return (
+      <Carousel  
+        additionalTransfrom={0}
+        arrows
+        autoPlaySpeed={3000}
+        centerMode={false}
+        className=""
+        containerClass="container-with-dots"
+        dotListClass=""
+        draggable
+        focusOnSelect={false}
+        infinite
+        itemClass=""
+        keyBoardControl
+        minimumTouchDrag={80}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={responsive}		
+        showDots={false}
+        sliderClass=""
+        slidesToSlide={1}
+        swipeable>
+      {/* <div><MovieGrid /></div> */}
+      {/* <div>Item 2</div> */}
+    	{/* <MovieGrid /> */}
+		<div>
+			<img src="https://m.media-amazon.com/images/M/MV5BNTdhYjEzYTEtYTkwZC00NzgxLWI0ZWEtYmEyMGZhOWYwMjE2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg" class="imageTrans" />
+		</div>
+		<div>
+			<img src="https://m.media-amazon.com/images/M/MV5BNTdhYjEzYTEtYTkwZC00NzgxLWI0ZWEtYmEyMGZhOWYwMjE2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg" class="imageTrans" />
+		</div>
+		<div>
+			<img src="https://m.media-amazon.com/images/M/MV5BNTdhYjEzYTEtYTkwZC00NzgxLWI0ZWEtYmEyMGZhOWYwMjE2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg" class="imageTrans" />
+		</div>
+		<div>
+			<img src="https://m.media-amazon.com/images/M/MV5BNTdhYjEzYTEtYTkwZC00NzgxLWI0ZWEtYmEyMGZhOWYwMjE2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg" class="imageTrans" />
+		</div>
+		<div>
+			<img src="https://m.media-amazon.com/images/M/MV5BNTdhYjEzYTEtYTkwZC00NzgxLWI0ZWEtYmEyMGZhOWYwMjE2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg" class="imageTrans" />
+		</div>
+      </Carousel>
+    );
+  }
 }
  
 export default CarouselClass;
