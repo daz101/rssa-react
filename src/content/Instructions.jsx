7c30631d7f06188cfc5ba9bc058b9181a35a31d8
Shahan M <mehtabi@clemson.edu>
2021-03-01 12:31:11 -0500

    Replaced class with className

--- src/content/Instructions.jsx
+++ src/content/Instructions.jsx
@@ -74,33 +74,33 @@ percent={30}
 
       <div className="instructions-page">   
      
-        <div class="container">
-  <div class="row">
-    <div class="col-sm">
-    <div class="card ">
-  <img src="/Preference-rssa.png" class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title"> Select Preferences</h5>
-    <p class="card-text">Rate movies for the system to learn about your preferences. You can select "Get another option" to get a new movie option.
+        <div className="container">
+  <div className="row">
+    <div className="col-sm">
+    <div className="card ">
+  <img src="/Preference-rssa.png" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title"> Select Preferences</h5>
+    <p className="card-text">Rate movies for the system to learn about your preferences. You can select "Get another option" to get a new movie option.
                   </p>
   </div>
 </div>
     </div>
-    <div class="col-sm">
-    <div class="card">
-  <img src="/recommendation-rssa.png" class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Rate Recommendations</h5>
-    <p class="card-text">The system will provide recommendations and for each you will be asked to rate the recommendation..</p>
+    <div className="col-sm">
+    <div className="card">
+  <img src="/recommendation-rssa.png" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Rate Recommendations</h5>
+    <p className="card-text">The system will provide recommendations and for each you will be asked to rate the recommendation..</p>
   </div>
 </div>
     </div>
-    <div class="col-sm">
-    <div class="card">
-  <img src="/survey-rssa.png" class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Complete Survey</h5>
-    <p class="card-text">Lastly, you will be asked to complete a survey about your experience interacting with the system.</p>
+    <div className="col-sm">
+    <div className="card">
+  <img src="/survey-rssa.png" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Complete Survey</h5>
+    <p className="card-text">Lastly, you will be asked to complete a survey about your experience interacting with the system.</p>
   </div>
 </div>
     </div>
@@ -123,5 +123,4 @@ percent={30}
     }
 }
 
- 
 export default Instructions;
