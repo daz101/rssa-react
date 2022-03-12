a315b1ae2c2ec60f39f9dfa3d5929cd3acc5043b
Shahan M <iShahanM@gmail.com>
2022-03-12 02:45:05 -0500

    Updated instructions page

--- src/content/pages/instructions.jsx
+++ src/content/pages/instructions.jsx
@@ -1,16 +1,15 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import { Redirect } from "react-router-dom";
-import axios from 'axios';
-import { API } from '../utils/constants';
 import { withMousePositionHook } from '../hooks/useMousePosition';
+import { API } from '../utils/constants';
 
 const surveyRssa = require("../res/survey-rssa.png");
 const prefRssa = require("../res/Preference-rssa.png");
 const recommendationRssa = require("../res/recommendation-rssa.png");
 
 class InstructionPage extends Component {
-
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -80,8 +79,8 @@ class InstructionPage extends Component {
 		return (
 			<>
 				<div className="jumbotron">
-					<h1 className="header">Overview</h1>
-					<p>The study consists of three steps.</p>
+					<h1 className="header">Instructions on how to use the system</h1>
+					<p>Using the movie recommender system consists of three steps.</p>
 				</div>
 				<div className="instructions-page">
 					<div className="row">
@@ -89,10 +88,11 @@ class InstructionPage extends Component {
 							<div className="card ">
 								<img src={prefRssa} className="card-img-top" alt="..." />
 								<div className="card-body">
-									<h5 className="card-title"> Select Preferences</h5>
+									<h5 className="card-title">Indicate Your Preference</h5>
 									<p className="card-text">
-										Rate movies for the system to learn about your
-										preferences. You can select "Get another option" to get a new movie option.
+										Please navigate through the endless list of movies and rate the
+										ones you have watched on a 5-point scale. The system will use these
+										ratings to learn about your preferences. Please rate at least 10 movies.
 									</p>
 								</div>
 							</div>
@@ -101,22 +101,24 @@ class InstructionPage extends Component {
 							<div className="card">
 								<img src={recommendationRssa} className="card-img-top" alt="..." />
 								<div className="card-body">
-									<h5 className="card-title">Rate Recommendations</h5>
+									<h5 className="card-title">Refine Your Recommendations</h5>
 									<p className="card-text">
-										The system will provide recommendations and for each
-										you will be asked to rate the recommendation.
+										The system will give you a first list of 7 recommendations and a list of 7
+										alternative items. Please rate all 14 items to refine the recommendations.
+										You will do this step <strong>twice</strong>.
 									</p>
 								</div>
 							</div>
 						</div>
 						<div className="col-sm">
 							<div className="card">
-								<img src={surveyRssa} className="card-img-top" alt="..." />
+								<img src={recommendationRssa} className="card-img-top" alt="..." />
 								<div className="card-body">
-									<h5 className="card-title">Complete Survey</h5>
+									<h5 className="card-title">Select A Movie to Watch</h5>
 									<p className="card-text">
-										Lastly, you will be asked to complete a survey about
-										your experience interacting with the system.
+										Once you have refined the recommendations twice, you will receive a final
+										set of 7 recommendations and 7 alternative items. Please select the one item
+										on this page that you are most interested in watching right now
 									</p>
 								</div>
 							</div>
