d5b91fc77ecd2c3f2d858b716ffb022a19eee446
Shahan M <iShahanM@gmail.com>
2022-07-07 14:13:16 -0400

    code splitting with lazy loading for multiple routes

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
+import { Component, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ERS from "./apps/ers";
-import PrefViz from "./apps/prefviz";
-import RSSA from "./apps/rssaApp";
-import RSSAStudy from "./apps/rssa";
-import RSSABaseline from "./apps/rssaBaseline";
 import LandingPage from "./landing";
-import CyberEd from "./apps/cybered";
 
+const ERS = lazy(() => import('./apps/ers'));
+const PrefViz = lazy(() => import('./apps/prefviz'));
+const RSSA = lazy(() => import('./apps/rssaApp'));
+const RSSAStudy = lazy(() => import('./apps/rssa'));
+const RSSABaseline = lazy(() => import('./apps/rssaBaseline'));
+const CyberEd = lazy(() => import('./apps/cybered'));
 
 class SurveyPortal extends Component {
 
@@ -15,15 +15,17 @@ class SurveyPortal extends Component {
 		return (
 			<>
 				<Router basename='/'>
-					<Routes>
-						<Route exact path="/" element={<LandingPage />}></Route>
-						<Route exact path="/rssaapp/*" element={<RSSA />}></Route>
-						<Route exact path="/cybered/*" element={<CyberEd />}></Route>
-						<Route path="/rssa/*" element={<RSSAStudy />}></Route>
-						<Route path="/rssabase/*" element={<RSSABaseline />}></Route>
-						<Route exact path="/ers/*" element={<ERS />}></Route>
-						<Route exact path="/prefviz/*" element={<PrefViz />}></Route>
-					</Routes>
+					<Suspense fallback={<div>Loading...</div>}>
+						<Routes>
+							<Route exact path="/" element={<LandingPage />}></Route>
+							<Route exact path="/rssaapp/*" element={<RSSA />}></Route>
+							<Route exact path="/cybered/*" element={<CyberEd />}></Route>
+							<Route path="/rssa/*" element={<RSSAStudy />}></Route>
+							<Route path="/rssabase/*" element={<RSSABaseline />}></Route>
+							<Route exact path="/ers/*" element={<ERS />}></Route>
+							<Route exact path="/prefviz/*" element={<PrefViz />}></Route>
+						</Routes>
+					</Suspense>
 				</Router>
 			</>
 		)
